7b2312cecf006b7b19ef1b0307652d588d5a4146
Justin Cooper <jcooper@chicousd.org>
2025-12-10 14:18:39 -0800

    removed terminationdate from SQL condistions

--- sql/retired-terminated.sql
+++ sql/retired-terminated.sql
@@ -12,14 +12,14 @@ SELECT
  EmploymentTypeDescr
 FROM vwHREmploymentList
 WHERE
- -- Code 1,2: Certificated and Classified. Code 4: Contracted
- PersonTypeId IN (1,2,4)
  -- R: Retired T: Terminated
- AND EmploymentStatusCode IN ('R','T')
+ EmploymentStatusCode IN ('R','T')
+ -- Code 1,2: Certificated and Classified. Code 4: Contracted
+ AND PersonTypeId IN (1,2,4)
  AND EmailWork LIKE '%@%'
  -- Has to have some kind of last day listed
- AND ( (DateTerminationLastDay IS NOT NULL) OR (DateTermination IS NOT NULL) )
+ -- AND ( (DateTerminationLastDay IS NOT NULL) OR (DateTermination IS NOT NULL) )
  -- Only get recently updated rows
- AND DateTimeEdited >= DATEADD(day, -30, GETDATE())
+ -- AND DateTimeEdited >= DATEADD(day, -30, GETDATE())
 -- ORDER BY DateTimeEdited;
 ORDER BY EmploymentStatusCode,empid
